4cb93af2dd2a9a83d55ef8f498ecd4d9644c13d2
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-01-06 18:43:37 +0100

    feat: add size adjusting of the IO folders

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from regex import P
 import whisper
 import datetime
 import os
+import glob
 import threading
 import queue
 import uuid
@@ -13,6 +15,36 @@ task_queue = queue.Queue(maxsize=3)
 
 # Dictionary to store task status
 tasks = {}
+
+def dir_size_adjust(dir_path, num_files=10, size_limit = 100000000):
+    """_summary_: Adjusts the number of files in the directory to 10 by deleting the oldest files.
+
+    Args:
+        file_path (str): Path to the directory.
+        num_files (int, optional): Number of files to keep. Defaults to 10.
+        size_limit (int, optional): Maximum size of the directory in bytes. Defaults to 100000000.
+    """
+    files = glob.glob(f"{dir_path}*")
+    try:
+        if len(files) > num_files:
+            files.sort(key=os.path.getmtime)
+            for i in range(len(files) - 10):
+                os.remove(files[i])
+            files.sort(key=os.path.getmtime, reverse=True)
+            return True
+        if sum(os.path.getsize(f) for f in files) > size_limit:
+            files.sort(key=os.path.getmtime)
+            while sum(os.path.getsize(f) for f in files) > size_limit:
+                os.remove(files[0])
+            files.sort(key=os.path.getmtime, reverse=True)
+            return True
+        else:
+            files.sort(key=os.path.getmtime, reverse=True)
+            return True
+    except Exception as e:
+        print(e)
+        return False
+
 
 def translate_speech(file_path, task_id):
     """_summary_: Translates speech from an audio file to text and stores the result in the tasks dictionary.
@@ -29,6 +61,7 @@ def translate_speech(file_path, task_id):
         file_name = file_path.split("/")[-1].split(".")[0] + ".txt"
         with open(f"runs\{file_name}", "w") as file:
             file.write(result['text'])
+        dir_size_adjust("runs")
     except Exception as e:
         tasks[task_id]['status'] = 'failed'
         tasks[task_id]['result'] = str(e)
@@ -56,7 +89,7 @@ def translate():
         400: Bad request.
         404: Task ID not found.
         429: Too many requests.
-        500: Internal server error.
+        500: Internal server error. Ergo something is wrong with this app.py file.
     
     Returns:
         JSON: JSON object containing the task ID.
@@ -75,7 +108,7 @@ def translate():
     filename = f"upload-{timestamp}{file_extension}"
     file_path = os.path.join("uploads/", filename)
     file.save(file_path)
-
+    dir_size_adjust("uploads")
     task_id = str(uuid.uuid4())
     tasks[task_id] = {'status': 'pending', 'result': None}
     task_queue.put((task_id, file_path))
@@ -130,5 +163,4 @@ if __name__ == '__main__':
         os.makedirs("uploads")
     if not os.path.exists("runs"):
         os.makedirs("runs")    
-    
     app.run(debug=True) # Remove debug=True when deploying to production
