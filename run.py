e1489676099b8796f42b15325f3a514a76f2e0a7
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-01-08 22:51:06 +0100

    feat: add more options to run.py, refactor to use environ vars

--- run.py
+++ run.py
@@ -6,9 +6,15 @@ import os
 parser = argparse.ArgumentParser(description="Run the app.")
 parser.add_argument("--port", type=int, default=8000, help="Port to run the app on.")
 parser.add_argument("--debug", action="store_true", help="Run the app in debug mode.")
+parser.add_argument("--noreload", action="store_false", help="Reload the app when the code changes.")
+parser.add_argument("--host", type=str, default="127.0.0.1", help="Host to run the app on. If you do not know what to put here, do not include this option.")
 
 # Command to be executed
-command = "uvicorn app:app --reload"
+command = "uvicorn app:app"
+
+# Default port
+os.environ["PORT"] = "8000"
+os.environ["HOST"] = "127.0.0.1"
 
 if __name__ == "__main__":
     print(f"Starting the app on {platform.system()}...")
@@ -16,7 +22,22 @@ if __name__ == "__main__":
     # Parse the arguments
     args = parser.parse_args()
     if args.port:
-        command += f" --port {args.port}"
+        if args.port < 1024 or args.port > 65535:
+            print("Port number must be between 1024 and 65535.")
+        else:
+            command += f" --port {args.port}"
+            os.environ["PORT"] = str(args.port)
+    if args.host:
+        if args.host == "127.0.0.1":                # localhost
+            command += f" --host {args.host}"
+        elif args.host == "0.0.0.0":                # any interface
+            command += f" --host {args.host}"
+        else:                                       # custom host                  
+            print("Custom host set, hope you know what you are doing.")
+            command += f" --host {args.host}"
+        os.environ["HOST"] = args.host
+    if args.noreload:
+        command += " --reload"
     if args.debug:
         os.environ["LOG_LEVEL"] = "DEBUG"
     if not args.debug:
